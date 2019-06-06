6a55344559d44bb0bcfe0fb929f1c71c85b3fb96
Jan Buschtöns <jan.buschtoens@clark.de>
2019-06-06 21:12:28 +0200

    feat: add babel transform

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,26 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    babel: {
+      plugins: [
+        [
+          require.resolve('./lib/babel-plugin-force-eager-initialization'),
+          {
+            imports: { '@glimmer/tracking': ['tracked'] },
+            globals: ['Ember._tracked']
+          }
+        ]
+      ]
+    },
+
     'ember-cli-babel': {
+      // This ensures that we don't actually write any non-parallelizable
+      // transforms.
+      // @see https://github.com/babel/ember-cli-babel#parallel-builds
+      throwUnlessParallelizable: true,
+
+      // This includes the "broken" `Symbol` polyfill.
+      // @see https://github.com/emberjs/ember.js/issues/18075#issuecomment-499549008
       includePolyfill: true
     }
   });
