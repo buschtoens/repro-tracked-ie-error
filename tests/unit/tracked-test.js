7eb21a01a7847f711d8a59c4ada8857359c5c4fc
Jan Buschtöns <jan.buschtoens@clark.de>
2019-06-06 16:48:29 +0200

    typo: applys -> applies

--- tests/unit/tracked-test.js
+++ tests/unit/tracked-test.js
@@ -5,7 +5,7 @@ import { tracked } from '@glimmer/tracking';
 module('Unit | @tracked', function(hooks) {
   setupTest(hooks);
 
-  test('it applys the initializer', function(assert) {
+  test('it applies the initializer', function(assert) {
     class Subject {
       @tracked
       property = 'foo';
